4b87a5dacc30b124503586aa274fbb2d13b4e99f
man7698 <matheusalmeida7698@gmail.com>
2021-06-12 00:24:26 -0300

    Add files via upload

---
+++ ValidarEmail/ValidarEmail/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ValidarEmail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string expressaoregular = "^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+
+            if(Regex.IsMatch(txtemail.Text,expressaoregular) == true)
+            {
+                MessageBox.Show("O E-mail esta correto !!!");
+            }
+            else
+            {
+                MessageBox.Show("O E-mail esta incorreto !!!");
+            }
+
+
+           
+
+
+        }
+    }
+}
